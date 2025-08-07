b785bfdf0ec34be9f364e4903685b99384103a29
Welington <wellingtonfabi@gmail.com>
2025-08-06 22:11:05 -0300

    adicionado Cron

--- src/main/java/com/extremebikebffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/extremebikebffagendadortarefas/infrastructure/client/EmailClient.java
@@ -3,11 +3,13 @@ package com.extremebikebffagendadortarefas.infrastructure.client;
 
 import com.extremebikebffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviaEmail(@RequestBody TarefasDTOResponse dto);
 
 }
